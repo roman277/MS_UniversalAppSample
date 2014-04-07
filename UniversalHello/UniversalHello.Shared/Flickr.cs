6afcbb8859c8c9c3efef7f97d68275a33d4d27e5
shwars <dmitri@soshnikov.com>
2014-04-07 15:08:53 +0400

    Revert "Исходная фиксация для добавления файлов по умолчанию .gitIgnore и .gitAttribute."
    This reverts commit 1444ea7b5ff33e3658c4f1ba26f4d9ae22ace70e.

---
+++ UniversalHello/UniversalHello.Shared/Flickr.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using System.Linq;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace UniversalHello
+{
+        public class Flickr
+        {
+
+        List<BitmapImage> list = new List<BitmapImage>();
+
+        public Flickr()
+        {
+            var xdoc = XDocument.Load("http://api.flickr.com/services/feeds/photos_public.gne");
+            XNamespace xn = "http://www.w3.org/2005/Atom";
+            var res = from z in xdoc.Descendants(xn + "entry")
+                        let l =
+                            (from x in z.Descendants(xn + "link")
+                            where x.Attribute("rel").Value == "enclosure"
+                            select x.Attribute("href").Value).FirstOrDefault()
+                        where (l!=null) && (l!="")
+                        select l;
+
+            foreach (var x in res)
+            {
+                list.Add(new BitmapImage(new Uri(x)));
+            }
+        }
+
+        public List<BitmapImage> Images
+        {
+            get
+            {
+                return list;
+            }
+        }
+
+    }
+}
